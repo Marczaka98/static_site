1d1891c1002d73aff5ff347adda9860a62393220
Marczaka98 <austin.marczak98@gmail.com>
2024-06-04 23:21:43 -0400

    updated parent constructor to pass the proper values

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,7 +2,7 @@ from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
     def __init__(self, value, tag=None, props=None):
-        super().__init__(x)
+        super().__init__(tag, value, None, props)
         self.tag = tag
         self.value = value
         self.props = props
