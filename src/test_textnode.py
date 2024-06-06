659705d3a53b57eaae9f9514f6adc50ad85ef4f9
Marczaka98 <austin.marczak98@gmail.com>
2024-06-06 10:38:23 -0400

    added split_nodes_delimiter and splitting_nodes functions

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import *
 
 class TestTextNode(unittest.TestCase):
     # def test_all_eq(self):
@@ -26,21 +26,31 @@ class TestTextNode(unittest.TestCase):
     #     node2 = TextNode("This is a text node", "bold", "!https://www.boot.dev")
     #     print(f"Testing Text Not Equal For:\n{node}\n{node2}")
     #     self.assertEqual(node, node2)
-    def test_types(self):
-        node = TextNode("This is a text node", "text", "https://www.boot.dev")
-        node2 = TextNode("This is a bold text node", "bold", "https://www.boot.dev")
-        node3 = TextNode("This is an italic text node", "italic", "https://www.boot.dev")
-        node4 = TextNode("This is a code node", "code", "https://www.boot.dev")
-        node5 = TextNode("This is a link node", "link", "https://www.boot.dev")
-        node6 = TextNode("This is an image node", "image", "https://www.boot.dev")
-        node7 = TextNode("This is an invalid node", "header", "https://www.boot.dev")
-        print(node.text_note_to_html_node().to_html())
-        print(node2.text_note_to_html_node().to_html())
-        print(node3.text_note_to_html_node().to_html())
-        print(node4.text_note_to_html_node().to_html())
-        print(node5.text_note_to_html_node().to_html())
-        print(node6.text_note_to_html_node().to_html())
-        print(node7.text_note_to_html_node().to_html())
+    # def test_types(self):
+    #     node = TextNode("This is a text node", "text", "https://www.boot.dev")
+    #     node2 = TextNode("This is a bold text node", "bold", "https://www.boot.dev")
+    #     node3 = TextNode("This is an italic text node", "italic", "https://www.boot.dev")
+    #     node4 = TextNode("This is a code node", "code", "https://www.boot.dev")
+    #     node5 = TextNode("This is a link node", "link", "https://www.boot.dev")
+    #     node6 = TextNode("This is an image node", "image", "https://www.boot.dev")
+    #     node7 = TextNode("This is an invalid node", "header", "https://www.boot.dev")
+    #     print(node.text_note_to_html_node().to_html())
+    #     print(node2.text_note_to_html_node().to_html())
+    #     print(node3.text_note_to_html_node().to_html())
+    #     print(node4.text_note_to_html_node().to_html())
+    #     print(node5.text_note_to_html_node().to_html())
+    #     print(node6.text_note_to_html_node().to_html())
+    #     print(node7.text_note_to_html_node().to_html())
+
+    def test_split_nodes(self):
+        node = TextNode("This is text with a **bold block** word", text_type_text)
+        node2 = TextNode("This is text with an *italic block* word", text_type_text)
+        node3 = TextNode("This is text with a `code block` word", text_type_text)
+        # new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        print(f"{node}\n{node2}\n{node3}")
+        print(split_nodes_delimiter([node, node2, node3], "**", text_type_bold))
+        print(split_nodes_delimiter([node, node2, node3], "*", text_type_italic))
+        print(split_nodes_delimiter([node, node2, node3], "`", text_type_code))
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,12 @@
 from leafnode import LeafNode
 
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode:
     def __init__(self, text, text_type, url = None):
         self.text = text
@@ -18,17 +25,43 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
     
-    def text_note_to_html_node(self):
-        if self.text_type == "text":
-            return LeafNode(None, self.text)
-        if self.text_type == "bold":
-            return LeafNode("b", self.text)
-        if self.text_type == "italic":
-            return LeafNode("i", self.text)
-        if self.text_type == "code":
-            return LeafNode("code", self.text)
-        if self.text_type == "link":
-            return LeafNode("a", self.text, {"href": self.url})
-        if self.text_type == "image":
-            return LeafNode("img", "", {"src": self.url, "alt": self.text})
-        raise ValueError(f"Invalid text type: {self.text}")
+def text_note_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == text_type_image:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            new_nodes.append(node)
+        if node.text.count(delimiter) % 2 != 0:
+            raise Exception(f"Missing matching delimiter in {node.text}")
+        node_text = node.text
+        if text_type == text_type_bold:
+            new_nodes.extend(splitting_nodes(node_text, delimiter, text_type_bold))
+        elif text_type == text_type_italic:
+            new_nodes.extend(splitting_nodes(node_text, delimiter, text_type_italic))
+        elif text_type == text_type_code:
+            new_nodes.extend(splitting_nodes(node_text, delimiter, text_type_code))
+    return new_nodes
+
+def splitting_nodes(text, delimiter, text_type):
+    if delimiter in text:
+        text_split = text.split(delimiter)
+        return [
+            TextNode(text_split[0], text_type_text), 
+            TextNode(text_split[1], text_type), 
+            TextNode(text_split[2], text_type_text)
+        ]
+    return [TextNode(text, text_type_text)]
