83eadde4777fa19528df03689864878293036404
Marczaka98 <austin.marczak98@gmail.com>
2024-06-08 16:20:13 -0400

    added new function markdown_to_html_node to process full markdown document into nodes

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -38,15 +38,15 @@ def block_to_block_type(block):
 def heading_block_to_htmlnode(block, block_type):
     heading_tag = f"h{block.count('#')}"
     block_text = re.sub(r"^\#{1,6} ", '', block)
-    return(HTMLNode(heading_tag, block_text))
+    return [HTMLNode(heading_tag, block_text)]
 
 def code_block_to_htmlnode(block, block_type):
     block_text = block.replace("```\n",'').replace("\n```",'')
-    return HTMLNode("pre", None, [HTMLNode("code", block_text)])
+    return [HTMLNode("pre", None, HTMLNode("code", block_text))]
 
 def quote_block_to_htmlnode(block, block_type):
     block_text = block.replace(">",'')
-    return HTMLNode("blockquote", block_text)
+    return [HTMLNode("blockquote", block_text)]
 
 def ulist_block_to_htmlnode(block, block_type):
     block_text = block.replace("* ", '')
@@ -54,7 +54,7 @@ def ulist_block_to_htmlnode(block, block_type):
     line_list = []
     for line in block_list:
         line_list.append(HTMLNode("li", line))
-    return HTMLNode("ul", None, line_list)
+    return [HTMLNode("ul", None, line_list)]
 
 def olist_block_to_htmlnode(block, block_type):
     block_list = block.split("\n")
@@ -62,7 +62,26 @@ def olist_block_to_htmlnode(block, block_type):
     for i in range(len(block_list)):
         block_list[i] = block_list[i].replace(f"{i+1}. ", '')
         line_list.append(HTMLNode("li", block_list[i]))
-    return HTMLNode("ol", None, line_list)
+    return [HTMLNode("ol", None, line_list)]
 
 def paragraph_block_to_htmlnode(block, block_type):
-    return HTMLNode("p", block)
+    return [HTMLNode("p", block)]
+
+def markdown_to_html_node(markdown):
+    block_list = markdown_to_blocks(markdown)
+    node_list = []
+    for block in block_list:
+        block_type = block_to_block_type(block)
+        if block_type == block_type_heading:
+            node_list.append(heading_block_to_htmlnode(block, block_type))
+        if block_type == block_type_code:
+            node_list.append(code_block_to_htmlnode(block, block_type))
+        if block_type == block_type_quote:
+            node_list.append(quote_block_to_htmlnode(block, block_type))
+        if block_type == block_type_ulist:
+            node_list.append(ulist_block_to_htmlnode(block, block_type))
+        if block_type == block_type_olist:
+            node_list.append(olist_block_to_htmlnode(block, block_type))
+        if block_type == block_type_paragraph:
+            node_list.append(paragraph_block_to_htmlnode(block, block_type))
+    return HTMLNode("div", None, node_list)
