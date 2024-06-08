9f137bade772859734e0a6381333fc18278dc7aa
Marczaka98 <austin.marczak98@gmail.com>
2024-06-08 01:49:11 -0400

    added block_to_block_type function with testing

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,12 @@
+import re
+
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_ulist = "unordered list"
+block_type_olist = "ordered list"
+block_type_paragraph = "paragraph"
+
 def markdown_to_blocks(markdown):
     block_list = markdown.split('\n\n')
     filtered_list = []
@@ -7,3 +16,20 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_list.append(block)
     return filtered_list
+
+def block_to_block_type(block):
+    block_lines = block.split("\n")
+    if re.findall(r"^\#{1,6} .+?", block_lines[0]) != []:
+        return block_type_heading
+    if len(block_lines) > 1 and block_lines[0].startswith("```") and block_lines[-1].startswith("```"):
+        return block_type_code
+    if re.findall(r"^>.+?", block_lines[0]) != []:
+        return block_type_quote
+    if re.findall(r"^\* .+?", block_lines[0]) != [] or re.findall(r"^\- .+?", block_lines[0]) != []:
+        return block_type_ulist
+    if re.findall(r"^1\. .+?", block_lines[0]) != []:    
+        for i in range(1,len(block_lines)):
+            if re.findall(r"^"+str(i+1)+"\. .+?", block_lines[i]) == []:
+                return block_type_paragraph
+        return block_type_olist
+    return block_type_paragraph
