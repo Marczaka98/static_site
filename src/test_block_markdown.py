5a395ac0910ade84e20c099a006eafc2b7421d3f
Marczaka98 <austin.marczak98@gmail.com>
2024-06-08 00:54:38 -0400

    created new testing file for block_markdown

---
+++ src/test_block_markdown.py
@@ -0,0 +1,11 @@
+from block_markdown import *
+
+
+markdown = """This is **bolded** paragraph
+
+This is another paragraph with *italic* text and `code` here
+This is the same paragraph on a new line
+
+* This is a list
+* with items"""
+print(markdown_to_blocks(markdown))
