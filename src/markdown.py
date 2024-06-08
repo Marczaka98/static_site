dfcfdd29de4815a3484a660a829cf37d4f833fc4
Marczaka98 <austin.marczak98@gmail.com>
2024-06-08 00:39:07 -0400

    Added text_to_textnodes function and cleaned up bug in other functions (split_nodes_image & split_nodes_link)

--- src/markdown.py
+++ src/markdown.py
@@ -1,12 +1,5 @@
-from textnode import TextNode
+from textnode import *
 import re
-
-text_type_text = "text"
-text_type_bold = "bold"
-text_type_italic = "italic"
-text_type_code = "code"
-text_type_link = "link"
-text_type_image = "image"
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -38,6 +31,9 @@ def split_nodes_image(old_nodes):
     new_nodes = []
     for node in old_nodes:
         splits = []
+        if node.url is not None:
+            new_nodes.append(node)
+            continue
         sections = re.split(r"(!\[.*?\]\(.*?\))", node.text)
         for i in range(len(sections)):
             if sections[i] == '':
@@ -46,7 +42,7 @@ def split_nodes_image(old_nodes):
                 image_tup = extract_markdown_images(sections[i])
                 splits.append(TextNode(image_tup[0][0], text_type_image, image_tup[0][1]))
             else:
-                splits.append(TextNode(sections[i], text_type_text))
+                splits.append(TextNode(sections[i], node.text_type))
         new_nodes.extend(splits)
     return new_nodes
 
@@ -54,6 +50,9 @@ def split_nodes_link(old_nodes):
     new_nodes = []
     for node in old_nodes:
         splits = []
+        if node.url is not None:
+            new_nodes.append(node)
+            continue
         sections = re.split(r"(\[.*?\]\(.*?\))", node.text)
         for i in range(len(sections)):
             if sections[i] == '':
@@ -62,6 +61,15 @@ def split_nodes_link(old_nodes):
                 image_tup = extract_markdown_links(sections[i])
                 splits.append(TextNode(image_tup[0][0], text_type_link, image_tup[0][1]))
             else:
-                splits.append(TextNode(sections[i], text_type_text))
+                splits.append(TextNode(sections[i], node.text_type))
         new_nodes.extend(splits)
+    return new_nodes
+
+def text_to_textnodes(text):
+    node = TextNode(text, "text")
+    new_nodes = split_nodes_delimiter([node], '**', text_type_bold)
+    new_nodes = split_nodes_delimiter(new_nodes, '*', text_type_italic)
+    new_nodes = split_nodes_delimiter(new_nodes, '`', text_type_code)
+    new_nodes = split_nodes_image(new_nodes)
+    new_nodes = split_nodes_link(new_nodes)
     return new_nodes
