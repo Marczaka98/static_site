5a2f0fce2a28659aa11300721b8be74b4e8b8138
Marczaka98 <austin.marczak98@gmail.com>
2024-06-08 16:55:37 -0400

    commented out test_block_markdown.py

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,24 +1,56 @@
-from block_markdown import *
+# from block_markdown import *
 
 
-# markdown = """This is a **bolded** paragraph
+# # markdown = """This is a **bolded** paragraph
 
-# This is another paragraph with *italic* text and `code` here
-# This is the same paragraph on a new line
+# # This is another paragraph with *italic* text and `code` here
+# # This is the same paragraph on a new line
 
-# * This is a list
-# * with items"""
-# print(markdown_to_blocks(markdown))
+# # * This is a list
+# # * with items"""
+# # print(markdown_to_blocks(markdown))
 
-# block = "####### This is a heading"
-block = """```
-This is a code block
-```"""
-# block = ">This is a quote"
-# block = """* This is an unordered list
-# * This is another line of the list"""
-# block = """1. This is an ordered list
+# # block = "# This is a heading"
+# # block = """```
+# # This is a code block
+# # This is the second line
+# # This is the third line
+# # ```"""
+# # block = """>This is a quote
+# # >This is the second line
+# # >This is the third line"""
+# # block = """* This is an unordered list
+# # * This is the second line
+# # * This is the third line"""
+# # block = """1. This is an ordered list
+# # 2. This is line 2
+# # 3. This is line 3"""
+
+# markdown = """# This is a heading
+
+# ```
+# This is a code block
+# This is the second line
+# This is the third line
+# ```
+
+# >This is a quote
+# >This is the second line
+# >This is the third line
+
+# * This is an unordered list
+# * This is the second line
+# * This is the third line
+
+# 1. This is an ordered list
 # 2. This is line 2
 # 3. This is line 3
-# 4. This is line 4"""
-print(block_to_block_type(block))
+
+# This is a paragraph
+# with a second line
+# and a third line"""
+
+# # block_type = block_to_block_type(block)
+# # print(paragraph_block_to_htmlnode(block, block_type))
+
+# print(markdown_to_html_node(markdown))
