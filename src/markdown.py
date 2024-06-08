7b7857b13c66f59ffcea7cb56d4fa07cbe752ad8
Marczaka98 <austin.marczak98@gmail.com>
2024-06-07 23:34:47 -0400

    Cleaned up split_nodes_delimiter function

--- src/markdown.py
+++ src/markdown.py
@@ -13,29 +13,30 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
         if node.text_type != text_type_text:
             new_nodes.append(node)
+            continue
         if node.text.count(delimiter) % 2 != 0:
             raise Exception(f"Missing matching delimiter in {node.text}")
-        node_text = node.text
-        if text_type == text_type_bold:
-            new_nodes.extend(splitting_nodes(node_text, delimiter, text_type_bold))
-        elif text_type == text_type_italic:
-            new_nodes.extend(splitting_nodes(node_text, delimiter, text_type_italic))
-        elif text_type == text_type_code:
-            new_nodes.extend(splitting_nodes(node_text, delimiter, text_type_code))
+        splits = []
+        sections = node.text.split(delimiter)
+        for i in range(len(sections)):
+            if sections[i] == '':
+                continue
+            if i % 2 == 0:
+                splits.append(TextNode(sections[i], text_type_text))
+            else:
+                splits.append(TextNode(sections[i], text_type))
+        new_nodes.extend(splits)
     return new_nodes
-
-def splitting_nodes(text, delimiter, text_type):
-    if delimiter in text:
-        text_split = text.split(delimiter)
-        return [
-            TextNode(text_split[0], text_type_text), 
-            TextNode(text_split[1], text_type), 
-            TextNode(text_split[2], text_type_text)
-        ]
-    return [TextNode(text, text_type_text)]
 
 def extract_markdown_images(text):
     return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
 def extract_markdown_links(text):
     return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        image = extract_markdown_images(node.text)
+        word_list = re.split(r"!\[(.*?)\]\((.*?)\)", node.text)
+        return word_list
